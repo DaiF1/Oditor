572ace2f019c07b9a8622a21a11a5859defc0bef
DaiF <julie.fiadino@gmail.com>
2022-07-14 22:45:35 +0200

    Main loop and read chars from stdin

--- oditor.ml
+++ oditor.ml
@@ -1,1 +1,21 @@
+(* Return a termios for reading input.
+    Copy of stdin with disabled echo and waiting for return key *)
+let raw_term () =
+    let term = Unix.tcgetattr Unix.stdin in
+    Unix.tcsetattr Unix.stdin Unix.TCSADRAIN
+        { term with Unix.c_icanon = false; Unix.c_echo = false};
+    term;;
 
+(* Read a char from given termios
+    Return the read char *)
+let get_char term = 
+    let res = input_char stdin in
+    Unix.tcsetattr Unix.stdin Unix.TCSADRAIN term;
+    res
+
+let rec loop () =
+    let term = raw_term () in
+    if get_char term == 'q' then ()
+    else loop ();;
+
+let () = loop ();;

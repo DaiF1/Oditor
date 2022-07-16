00413abafc3ea68bf6253ce12c3ce185012303e2
DaiF <julie.fiadino@gmail.com>
2022-07-16 23:05:54 +0200

    Add prints to main loop

--- oditor.ml
+++ oditor.ml
@@ -2,7 +2,7 @@
 let term = Unix.tcgetattr Unix.stdin;;
 
 (* Open terminal raw mode
-    Terminal with disabled echo and no return needed to read buffer *)
+    Terminal with disabled echo and read byte by byte *)
 let enter_raw () =
     Unix.tcsetattr Unix.stdin Unix.TCSADRAIN
         { term with Unix.c_icanon = false; Unix.c_echo = false};;
@@ -17,10 +17,25 @@ let exit_raw () =
 let get_char () = 
     input_char stdin;;
 
+(* Return true if char is printable *)
+let printable c =
+    let c = Char.code c in
+    c >= 32 && c < 127;;
+
 (* Main loop
     Quit if letter 'q' pressed *)
 let rec loop () =
-    if get_char () == 'q' then exit_raw ()
-    else loop ();;
+    let c = get_char () in
+    if c == 'q' then exit_raw ()
+    else
+        begin
+            begin
+                if printable c then
+                    print_endline (Printf.sprintf "%d ('%c')" (Char.code c) c)
+                else
+                    print_endline (Printf.sprintf "%d" (Char.code c))
+            end;
+            loop ()
+        end;;
 
 let () = enter_raw (); loop ();;

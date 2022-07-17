a50f5e65d072d20c152f58174cf0bc9a6280a484
DaiF <julie.fiadino@gmail.com>
2022-07-17 12:53:41 +0200

    Separate keypress processing and reading

--- oditor.ml
+++ oditor.ml
@@ -31,33 +31,27 @@ let exit_raw () =
             Unix.c_istrip = true; Unix.c_csize = isize;
             Unix.c_vmin = imin; Unix.c_vtime = itime};;
 
-(* Read a char from given termios
-    Return the read char *)
-let get_char () = 
-    try input_char stdin
-    with End_of_file -> '\000';;
 
 (* Return true if char is printable *)
 let printable c =
     let c = Char.code c in
     c >= 32 && c < 127;;
 
+(* Read byte from stdin
+    Return the read byte *)
+let read_key () = 
+    try input_char stdin
+    with End_of_file -> '\000';;
+
+(* Process key presses*)
+let process_key () = match read_key with
+    | c when c = ctrl 'q' -> exit_raw (); 1
+    | _ -> 0;;
+
+
 (* Main loop
     Quit if ctrl+q is pressed *)
 let rec loop () =
-    let c = get_char () in
-    if c = ctrl 'q' then exit_raw ()
-    else
-        begin
-            begin
-                if printable c then
-                    (Printf.printf "%d ('%c')\r\n" (Char.code c) c;
-                    flush stdout)
-                else
-                    (Printf.printf "%d\r\n" (Char.code c);
-                    flush stdout)
-            end;
-            loop ()
-        end;;
+    if process_key () = 0 then loop ();;
 
 let () = enter_raw (); loop ();;

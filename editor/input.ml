315f0223046da0b227f0c4880c99da90e337e3a8
DaiF <julie.fiadino@gmail.com>
2022-07-29 20:40:43 +0200

    Fix wrong line length when forcing cursor on text

--- editor/input.ml
+++ editor/input.ml
@@ -50,7 +50,7 @@ let move_cursor key =
         | 'j' -> move_cy 1
         | _ -> ()
     in toggle_cursor true; process (); 
-        let linelen = (get_line term.y).size in
+        let linelen = (get_line (term.y + term.rowoff)).size in
         term.x <- if term.x >= linelen then linelen - 1
                 else term.x;
         toggle_cursor false;;

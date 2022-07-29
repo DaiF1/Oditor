f05665168e371046fba456b7f3a354cc560bf176
DaiF <julie.fiadino@gmail.com>
2022-07-29 22:01:00 +0200

    Status bar invert mode display color

--- editor/display.ml
+++ editor/display.ml
@@ -19,12 +19,12 @@ let draw_rows () =
         let completion = if term.numlines = 0 then 100
             else int_of_float (float_of_int term.y /. 
                 float_of_int (term.numlines - 1) *. 100.0) 
-        in let status = "\x1b[1m" ^ string_of_mode term.mode ^ "\x1b[0m " ^
+        in let status = "\x1b[7m\x1b[1m " ^ string_of_mode term.mode ^ " \x1b[0m " ^
             term.filename 
         and stats = "line " ^ string_of_int term.y ^ " (" ^
             string_of_int completion ^ "%)" in
-        (* the '+8' is to nullify the escape codes for bold text *)
-        let offset = (term.cols - String.length status - String.length stats + 8)
+        (* the '+11' is to nullify the escape codes for formatting text *)
+        let offset = (term.cols - String.length status - String.length stats + 11)
         in status ^ String.make offset ' ' ^ stats
 
     in let cut_lign line off =
